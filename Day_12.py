d8fb70f73c35a9d1fa342585e4d7eda7d35e8547
magicorganizer <holger.mackeldey@googlemail.com>
2022-12-18 10:39:26 +0100

    some prints

--- Day_12.py
+++ Day_12.py
@@ -8,7 +8,8 @@ class Direction(Enum):
     UNDEFINED = 4
 
 class Track:
-    def __init__(self):
+    def __init__(self, track_name):
+        self.track_name = track_name
         self.pos = [0, 0]
         self.curr_height = chr(ord("a") - 1)
         self.track_status = "searching"  # finished, blocked
@@ -16,9 +17,20 @@ class Track:
         self.next_step_quality = 100
         self.track = []
 
-    def step(self):
-        pass
+    def __str__(self):
+        return f"Track {self.track_name} {self.pos} H:{self.curr_height} St:{self.track_status} Dir:{self.next_step} L:{self.track}"
 
+    def step(self):
+        self.track.append(self.pos)
+        if self.next_step == 0:
+            self.pos[0] -= 1
+        elif self.next_step == 1:
+            self.pos[1] -= 1
+        elif self.next_step == 2:
+            self.pos[0] += 1
+        elif self.next_step == 3:
+            self.pos[1] += 1
+        print(self.track_name, self.pos)
 class Day12:
     def __init__(self):
         self.commands = []
@@ -27,8 +39,9 @@ class Day12:
         self.map_width = 0
         self.map_height = 0
         self.curr_track_idx = 0
-        self.tracks = [Track()]
+        self.tracks = [Track("init_track")]
         self.track_backlog = []
+        self.back_track_idx = 1
 
     def load_puzzle_input(self):
         self.commands.clear()
@@ -70,44 +83,65 @@ class Day12:
         if len(list_of_dirs) > 1:
             self.map[pos[0]][pos[1]] = "+"
         else:
-            self.map[pos[0]][pos[1]] = list_of_chars[list_of_chars.pop(0)]
+            self.map[pos[0]][pos[1]] = list_of_chars[list_of_dirs.pop(0)]
+
+    def print_tracks(self):
+        print("print tracks")
+        for idx, track in enumerate(self.tracks):
+            print(idx, track)
 
     def find_track(self):
+        ret_val = False
         for track in self.tracks:
-            save_track_pos = track.pos
-            list_of_dirs = []
-            list_dir_effort = self.get_best_way(track.pos)
-            continue_curr_track = True
-            number_of_possible_dirs = 0
-            temp_track = track
-            for idx, dir in enumerate(list_dir_effort):
-                if dir != "X":
-                    delta = ord(dir) - ord(track.curr_height)
-                    if delta in [1, 0]:
-                        list_of_dirs.append(idx)
-                        number_of_possible_dirs += 1
-                        if continue_curr_track:
-                            track.next_step_quality = delta
-                            track.next_step = idx
-                            track.step()
-                            print(f"{Direction(idx).name} {delta}")
-                            continue_curr_track = False
-                        else:
-                            # copy track for next possible way
-                            new_track = temp_track
-                            new_track.next_step_quality = delta
-                            new_track.next_step = idx
-                            new_track.step()
-                            self.track_backlog.append(new_track)
-            if number_of_possible_dirs == 0:
-                track.track_status = "blocked"
-            self.mark_position_in_matrix(save_track_pos, list_of_dirs)
+            if track.track_status == "searching":
+                save_track_pos = track.pos
+                list_of_dirs = []
+                list_dir_effort = self.get_best_way(track.pos)
+                continue_curr_track = True
+                number_of_possible_dirs = 0
+                temp_track = track
+                for idx, dir in enumerate(list_dir_effort):
+                    if dir != "X":
+                        delta = ord(dir) - ord(track.curr_height)
+                        if delta in [1, 0]:
+                            list_of_dirs.append(idx)
+                            number_of_possible_dirs += 1
+                            if continue_curr_track:
+                                track.next_step_quality = delta
+                                track.next_step = idx
+                                print(f"{Direction(idx).name} {delta}")
+                                continue_curr_track = False
+                            else:
+                                # copy track for next possible way
+                                new_track = temp_track
+                                new_track.next_step_quality = delta
+                                new_track.next_step = idx
+                                new_track.track_name = f"back_track_{self.back_track_idx}"
+                                self.back_track_idx += 1
+                                self.track_backlog.append(new_track)
+                if number_of_possible_dirs == 0:
+                    track.track_status = "blocked"
+                self.mark_position_in_matrix(save_track_pos, list_of_dirs)
 
+        for back_track in self.track_backlog:
+            self.tracks.append(back_track)
+        self.track_backlog.clear()
+
+        for track in self.tracks:
+            track.step()
+            track.curr_height = self.map[track.pos[0]][track.pos[1]]
+            if track.track_status == "searching":
+                ret_val = True
         self.print_map()
+        return ret_val
 
 
 puzzle = Day12()
 puzzle.load_puzzle_input()
 puzzle.fill_map()
 puzzle.find_start_point()
-puzzle.find_track()
+while 1:
+    puzzle.print_tracks()
+    if not puzzle.find_track():
+
+        break
